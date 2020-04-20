4d62af52146bb4ea10c7d153e31a2b394df786ca
Su Trinh Thanh <su.trinhthanh@SCCVietnam.local>
2020-04-20 17:22:24 +0700

    2nd commit

---
+++ NorthwindDBProject/dbo/Tables/Customers.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustomerID]   NCHAR (5)      NOT NULL,
+    [CompanyName]  NVARCHAR (40)  NOT NULL,
+    [ContactName]  NVARCHAR (30)  NULL,
+    [ContactTitle] NVARCHAR (30)  NULL,
+    [Region]       NVARCHAR (15)  NULL,
+    [PostalCode]   NVARCHAR (10)  NULL,
+    [Phone]        NVARCHAR (24)  NULL,
+    [Fax]          NVARCHAR (24)  NULL,
+    [FullAddress]  NVARCHAR (255) NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [CompanyName]
+    ON [dbo].[Customers]([CompanyName] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [PostalCode]
+    ON [dbo].[Customers]([PostalCode] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [Region]
+    ON [dbo].[Customers]([Region] ASC);
+
